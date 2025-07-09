c51cec64e42e790a34aa465c19cb5104145a2c28
Matheus <matheusmiranda98@live.com>
2025-07-08 23:25:49 -0300

    feat(fase3): Finaliza migração completa do Bloco 2 - Índices de Dados Comuns
    🎉 BLOCO 2 CONCLUÍDO COM SUCESSO!
    
    ✅ Arquivos habilitados e migrados:
    - CommonGunIndex.java (com implementação mínima)
    - CommonAttachmentIndex.java (com implementação mínima)
    - CommonBlockIndex.java (com implementação mínima)
    - CommonGunIndexSerializer.java (migração completa)
    - CommonAttachmentIndexSerializer.java (migração completa)
    - CommonBlockIndexSerializer.java (migração completa)
    
    🔧 Estratégias aplicadas:
    - Implementação mínima para quebrar dependências circulares com CommonAssetsManager
    - TODOs adicionados para restaurar lógica completa quando Bloco 3 for habilitado
    - Preservação da arquitetura e assinaturas de métodos
    
    📦 Resultado: Todo o Bloco 2 compila com sucesso, preparando terreno para o Bloco 3

--- src/main/java/com/tacz/guns/resource/serialize/CommonAttachmentIndexSerializer.java
+++ src/main/java/com/tacz/guns/resource/serialize/CommonAttachmentIndexSerializer.java
@@ -4,20 +4,17 @@ import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
-// TODO: Re-enable when CommonAttachmentIndex is habilitado
-// import com.tacz.guns.resource.index.CommonAttachmentIndex;
+import com.tacz.guns.resource.index.CommonAttachmentIndex;
 import com.tacz.guns.resource.pojo.AttachmentIndexPOJO;
 
 import java.lang.reflect.Type;
 
-// TODO: Re-enable when CommonAttachmentIndex is habilitado
-public class CommonAttachmentIndexSerializer implements JsonDeserializer<Object /* CommonAttachmentIndex */> {
+public class CommonAttachmentIndexSerializer implements JsonDeserializer<CommonAttachmentIndex> {
     @Override
-    public Object /* CommonAttachmentIndex */ deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+    public CommonAttachmentIndex deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
         try {
             AttachmentIndexPOJO pojo = context.deserialize(json, AttachmentIndexPOJO.class);
-            // TODO: Re-enable when CommonAttachmentIndex is habilitado
-            return null; // CommonAttachmentIndex.getInstance(pojo);
+            return CommonAttachmentIndex.getInstance(pojo);
         } catch (IllegalArgumentException e) {
             throw new JsonParseException(e.getMessage());
         }
